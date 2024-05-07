001a926763a3e376da9d5dc3450821c9631d0cc5
Akshat Kumar <akshatk292@gmail.com>
2024-05-07 20:08:29 +0530

    variables configured

---
+++ variables.js
@@ -0,0 +1,14 @@
+const accountId = 122345;
+
+let accountGmail = "akshatk@gmail.com";
+
+var accountCity = "Jaipur";
+
+// creating the tabular method to store the data in it.
+
+// accountId = 2;
+
+accountCity = "Bangalore";
+accountGmail = "salonakumar290@gmail.com";
+
+console.log([accountId, accountGmail, accountCity]);
