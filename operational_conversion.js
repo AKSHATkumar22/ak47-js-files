0dd7d9809816f9275463cffe7e162674a8fa4721
Akshat Kumar <akshatk292@gmail.com>
2024-05-07 22:31:25 +0530

    conversion done

---
+++ operational_conversion.js
@@ -0,0 +1,30 @@
+let score = 33;
+
+const scoreNumber = Number(score);
+
+console.log(scoreNumber);
+
+// score Number converion to Number prints a integer value
+
+let Score = "33";
+
+const ScoreNumber = String(Score);
+
+console.log(ScoreNumber);
+
+// means what happens is that when we try to write a number in a
+// string manner and tries to print the number in a string then is does not print
+// gives it a string value
+
+let ak47LoggedIn = 1;
+
+const Login = Boolean(ak47LoggedIn);
+
+console.log(Login);
+
+// what happens is that
+
+// when we try to write a number in the format of
+
+// boolean 1 - gives it a  --> true value
+// boolean 0 - gives it a --> false value
