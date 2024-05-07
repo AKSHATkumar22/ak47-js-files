0434e49d6329aade33714cc97c5c4b52be7727a1
Akshat Kumar <akshatk292@gmail.com>
2024-05-08 00:40:32 +0530

    If-else pushed

---
+++ ConditionalStatements.js
@@ -0,0 +1,15 @@
+// let age = 18;
+
+// if (age == 18) {
+//   console.log("he can vote");
+// } else {
+//   console.log("he cannot vote");
+// }
+
+let age = 18;
+
+if (age > 18) {
+  console.log("he can vote");
+} else {
+  console.log("he cannot vote");
+}
