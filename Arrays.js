3826b774e16ce98f3405aedbaa623f5139c13b70
Akshat Kumar <akshatk292@gmail.com>
2024-05-08 00:34:56 +0530

    Arrays configured

---
+++ Arrays.js
@@ -0,0 +1,15 @@
+// to create the arrays
+const herores = [
+  "akshat kumar",
+  "neeli kumari",
+  "pihoo kumari",
+  "pratayasha kuamri",
+];
+
+let Obj = {
+  name: "akshat kumar",
+  age: "21",
+  email: "saklona292",
+};
+
+console.table(herores);
