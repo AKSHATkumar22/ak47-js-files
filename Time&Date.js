5beb1385939a0087e6d3250720da679c434ecb42
Akshat Kumar <akshatk292@gmail.com>
2024-05-08 23:19:12 +0530

    Time changes

--- Time&Date.js
+++ Time&Date.js
@@ -1,3 +1,11 @@
+// Date ---> using date stamp
+
 let myCreatedDate = new Date("01-22-2003");
 
-console.log(myCreatedDate.toLocaleDateString());
+// console.log(myCreatedDate.toLocaleDateString());
+
+// time stamp --->
+
+let myTimeStamp = Date.now();
+
+console.log(myTimeStamp);
