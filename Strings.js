fe14130e316a8594e3e76501996c6a3ca2bbb1d3
Akshat Kumar <akshatk292@gmail.com>
2024-05-08 19:10:39 +0530

    More strings methods

--- Strings.js
+++ Strings.js
@@ -3,3 +3,29 @@ const str1 = "1";
 const str2 = "2";
 
 console.log(str1 + str2);
+
+// new strings method
+
+const myName = "Akshat Kumar";
+
+console.log(`Hello my name is, ${myName}`);
+
+// now secondmethod in the strings is trim , char At , Splice, to Uppercase characters
+
+const happyName = "Akshat Kumar";
+
+console.log(`Hello today I am feeling low ${happyName.charAt(2)}`);
+
+// Now the third i.e toUpper case
+
+const mySadName = "You are good";
+
+console.log(
+  `Making the characters at upper case ${mySadName.toUpperCase(mySadName)}`
+);
+
+// now the 4th method is trim --> means cutting the un-needed value
+
+const myUseCase = "Phones";
+
+console.log(`Everyday I need my phone ${myUseCase.trim(myUseCase)}`);
