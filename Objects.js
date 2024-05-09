4b033f3f2ecf09a99cb9f1e89fc1f6f558648263
Akshat Kumar <akshatk292@gmail.com>
2024-05-09 18:23:36 +0530

    Some more changes

--- Objects.js
+++ Objects.js
@@ -1,5 +1,6 @@
 // Here, I have created the class name JsUser,
 // where it defines the block scope of a code or the structure of the code.
+// Basaically this are called the literals objects creation..
 const JsUser = {
   // Here, It denotes the object of the class --> where it means the instance of a class
   // or the member functions or the user-defined functions.
