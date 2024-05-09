7ef5a425fc4b554a439ee9037b881280ec5288c2
Akshat Kumar <akshatk292@gmail.com>
2024-05-09 19:12:57 +0530

    Functions

---
+++ Functions.js
@@ -0,0 +1,15 @@
+// Here, I am trying to print the function with the adding of the two variables storing int the function
+
+function myName(a, b) {
+  console.log(a + b);
+}
+
+myName(2, 5);
+
+// Different methods to write a function
+
+function myAbs() {
+  console.log(result);
+}
+
+myAbs(3);
