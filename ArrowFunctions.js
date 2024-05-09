3a1f40952bb09acbe555c38efe04304b7226ce9d
Akshat Kumar <akshatk292@gmail.com>
2024-05-09 22:49:36 +0530

    Arrow

---
+++ ArrowFunctions.js
@@ -0,0 +1,12 @@
+// Here, In this we will learn about the using of the arrow function
+const Ak47 = (num1, num2) => {
+  // when we are creating normal arrow function using curly brackets then we have to give the return value
+  //Here, I am returning the num1 + num2 value using the arrow function
+  return num1 + num2;
+  //here I have declared he arrow functions
+};
+console.log(Ak47(4, 7));
+// 2nd method ---> imperial function creation
+const addTwo = (a, b) => a + b;
+// Here, we are not giving curly brackets so we have to use imperial functions method...
+console.log(addTwo(3, 6));
